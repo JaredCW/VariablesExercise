a172bafa74353e2c97e3b1960f85329a91f1fdb0
Jared Conner <jared.w.conner@gmail.com>
2022-09-28 08:51:30 -0500

    Completed Exercise

---
+++ script.js
@@ -0,0 +1,44 @@
+// STEP ONE
+// "const" is used for non-changing variable types
+
+//STEP TWO
+let num = 20;
+
+//STEP THREE
+num = 15;
+
+// STEP FOUR
+num = num + 7;
+
+//STEP FIVE
+num --;
+
+//STEP SIX
+num = "This is not a number"
+
+//Console test for num
+// console.log(num);
+
+//STEP SEVEN
+let blank
+
+//STEP EIGHT
+console.log(blank);
+
+//STEP NINE
+blank = null
+
+
+
+//BONUS ONE
+const quarter = 0.25 , dime = 0.1 , nickel = 0.05 , penny = 0.01
+
+//BONUS TWO
+console.log(quarter, dime, nickel, penny);
+
+//BONUS THREE
+let letters = "qwertyuiop"
+
+//BONUS FOUR
+console.log(letters[letters.length - 1]);
+
