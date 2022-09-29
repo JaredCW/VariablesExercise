17961f280af4fe4680b027054396cc37972c9fc1
Jared Conner <jared.w.conner@gmail.com>
2022-09-29 05:08:03 -0500

    Updated notes for future ref.

--- script.js
+++ script.js
@@ -32,6 +32,8 @@ blank = null
 
 //BONUS ONE
 const quarter = 0.25 , dime = 0.1 , nickel = 0.05 , penny = 0.01
+// Another method (array-style)
+// const [quarter, dime, nickel, penny] = [0.25, 0.1, 0.05, 0.01]
 
 //BONUS TWO
 console.log(quarter, dime, nickel, penny);
